91ba619f3edf1d49ee73f442c0ba49bf53459c55
browbo01 <borisbrown0077@gmail.com>
2021-06-07 22:00:02 -0400

    initial commit

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,14 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 public class User {
+    @Size (min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+    @Email (message = "Must enter a valid email address.")
     private String email;
+    @Size (min = 6, message = "Password must be at least six characters in length.")
     private String password;
 
     public User() {
